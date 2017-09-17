2a7756a73520bf57673a5bf619d2382f3c57d518
Barry Peng <barrypeng6@hotmail.com>
2017-09-17 17:57:16 +0800

    add eslint

--- src/action.js
+++ src/action.js
@@ -4,18 +4,18 @@ const HANDLE_LOGOUT = 'HANDLE_LOGOUT';
 
 export function handleLogin() {
   return {
-    type: 'HANDLE_LOGIN'
+    type: HANDLE_LOGIN
   }
 }
 
 export function handleLogout() {
   return {
-    type: 'HANDLE_LOGOUT'
+    type: HANDLE_LOGOUT
   }
 }
 
 export function fetchStoreList() {
   return {
-    type: 'FETCH_STORE_LIST'
+    type: FETCH_STORE_LIST
   }
 }
