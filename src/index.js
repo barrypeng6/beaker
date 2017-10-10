c931d80c10050f9ca2087e29de91ce169baddc69
Barry Peng <barrypeng6@hotmail.com>
2017-10-10 16:31:08 +0800

    rename Root.js --> root.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root';
+import Root from './root';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import createSagaMiddleware from 'redux-saga';
