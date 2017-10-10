c2e420c3db1b226b658da4e4c661519f0ef32277
Barry Peng <barrypeng6@hotmail.com>
2017-10-10 21:14:53 +0800

    add comment

--- src/sagas/checkLogin.test.js
+++ src/sagas/checkLogin.test.js
@@ -1,32 +1,42 @@
-import { call, put } from 'redux-saga/effects';
-import { checkLogin } from './checkLogin';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { watchCheckLogin, checkLogin } from './checkLogin';
 import { callApiCheckLogin } from '../api';
 import {
+  CHECK_LOGIN_REQUEST,
   checkLoginSuccess,
   checkLoginFail
 } from '../actions';
 
 describe('sagas/ checkLogin', () => {
 
-  const iterator = checkLogin();
+  // 測試項目：收到 action: CHECK_LOGIN_REQUEST，是否有執行 fn: checkLogin
+  test('watch', () => {
+    const iterator = watchCheckLogin()
+    const expected = takeEvery(CHECK_LOGIN_REQUEST, checkLogin);
+    const actual = iterator.next().value;
+    expect(actual).toEqual(expected);
+  });
 
-  test('callApiCheckLogin', () => {
+  // 測試項目：是否有call正確的api
+  const iterator = checkLogin();
+  test('api request', () => {
     const expected = call(callApiCheckLogin);
     const actual = iterator.next().value;
     expect(actual).toEqual(expected);
   });
 
-  test('checkLogin success', () => {
+  // 測試項目：是否有正確處理 successful response
+  test('success', () => {
     const expected = put(checkLoginSuccess());
     const getResponse = () => ({ status: 200 })
     const actual = iterator.next(getResponse()).value;
     expect(actual).toEqual(expected);
   });
 
-  test('checkLogin fail', () => {
+  // 測試項目：是否有正確處理 failure response
+  test('fail', () => {
     const expected = put(checkLoginFail());
     const actual = iterator.throw('check login fial').value;
     expect(actual).toEqual(expected);
   });
-
 });
