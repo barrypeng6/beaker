5a0f769082e9c5b9e13242a7e9b157b89b324123
Barry <barrypeng6@hotmail.com>
2017-10-17 15:35:29 +0800

    add login form

--- src/scenes/Login/index.js
+++ src/scenes/Login/index.js
@@ -19,17 +19,59 @@ const mapDispatchToProps = (dispatch) => {
   };
 }
 
-class Login extends Component {
+const Login = ({ location, isLogin, isLogining, onLogin, error }) => {
+  const { from } = location.state || { from: { pathname: '/' } };
+  return (
+    isLogin ? <Redirect to={from}/>
+    : <LoginContainer
+      isLogining={isLogining}
+      onLogin={onLogin}
+      error={error}
+      from={from}
+    />
+  );
+}
+
+  class LoginContainer extends Component {
+    constructor() {
+      super();
+      this.state = {
+        account: 'annie@meepshop.com',
+        password: '123456'
+      }
+    }
+
+    _handleAccountInputChange = (e) => {
+      const newAccount = e.target.value;
+      this.setState(() => ({ account: newAccount }));
+    }
+
+    _handlePasswordInputChange = (e) => {
+      const newPassword = e.target.value;
+      this.setState(() => ({ password: newPassword }));
+    }
+
     render() {
-      const { location, isLogin, isLogining, onLogin, error } = this.props;
-      const { from } = location.state || { from: { pathname: '/' } };
+      const { from, isLogining, onLogin, error } = this.props;
+      const { account, password } = this.state;
       return (
-        isLogin ? <Redirect to={from}/>
-        : <div>
+        <div>
           <p>You must log in to view the page at {from.pathname}</p>
+          <input
+            type="text"
+            name={'account'}
+            value={account}
+            onChange={this._handleAccountInputChange}
+          />
+          <input
+            type="password"
+            name={'password'}
+            value={password}
+            onChange={this._handlePasswordInputChange}
+          />
           {isLogining ? <span>{'is logging...'}</span>
             : <button onClick={() => {
-                onLogin('annie@meepshop.com', '123456');
+                onLogin(account, password);
               }}
             >Log in</button>}
             <div style={{color: 'red'}}>{error}</div>
